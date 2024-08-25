f5bdde7f05952a95e641437179900ada5f38a94a
Roisin Dai <roisin@ideanotion.net>
2024-08-25 16:06:51 -0700

    java quiz initial functions and setup

---
+++ demo/src/main/java/com/PickPerfect/demo/model/Question.java
@@ -0,0 +1,24 @@
+package com.PickPerfect.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String questionTitle; // question_title
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String rightAnswer;
+    private String category;
+    private String difficultylevel; // difficultyLevel
+
+}
---
+++ demo/src/main/java/com/PickPerfect/demo/model/Response.java
@@ -0,0 +1,11 @@
+package com.PickPerfect.demo.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
