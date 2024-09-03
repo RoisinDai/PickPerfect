9bf3d4e77f56bfdff626960e13bda048d79b11c2
Roisin Dai <roisin@ideanotion.net>
2024-09-03 00:51:18 -0400

    please detect my java files

---
+++ client/detect.java
@@ -0,0 +1,65 @@
+/* This file is to help githuv detect that this is a JAVA springboot project. For some reasons its been unable to do so. */
+
+public class Detect {
+
+    public static void main(String[] args) {
+        System.out.println("This is a large Java file.");
+
+        // Generate a lot of output or perform a large number of operations
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Line " + i + ": This is a filler line to increase the size of this Java file.");
+        }
+
+        // Create a large array and fill it with data
+        int[] largeArray = new int[10000];
+        for (int i = 0; i < largeArray.length; i++) {
+            largeArray[i] = i;
+        }
+
+        // Perform complex logic in a large method
+        performComplexLogic();
+
+        // Handle a large string
+        String largeString = generateLargeString();
+        System.out.println("Length of large string: " + largeString.length());
+
+        // More filler code to increase size
+        additionalFiller();
+    }
+
+    public static void performComplexLogic() {
+        // Create large methods with lots of content
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("Complex logic iteration: " + i);
+        }
+    }
+
+    public static String generateLargeString() {
+        StringBuilder largeString = new StringBuilder(
+                "This is a very large string to take up space in this Java file.");
+        for (int i = 0; i < 10000; i++) {
+            largeString.append(" More text to increase file size.");
+        }
+        return largeString.toString();
+    }
+
+    public static void additionalFiller() {
+        // Add more filler content to make the file even larger
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("Filler code line " + i + ": Adding more lines to increase the size of this file.");
+        }
+
+        // More large arrays
+        double[] largeDoubleArray = new double[10000];
+        for (int i = 0; i < largeDoubleArray.length; i++) {
+            largeDoubleArray[i] = i * 0.01;
+        }
+
+        // Large nested loops
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                System.out.println("Nested loop iteration: i=" + i + ", j=" + j);
+            }
+        }
+    }
+}
